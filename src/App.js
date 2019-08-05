fae5f4632441e55ef803a9049d6f734a0e124846
collab-virchatri <trilokc@virtusa.com>
2019-08-05 12:59:15 +0530

    homepage crated with component, added menu-item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './homepage.style.scss';
+import HomePage from './pages/homepage/homepage.component';
 
-import HomePage from './homepage.component'
 function App() {
   return (
     <div >
