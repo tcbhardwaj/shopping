ad64d09d73a22eb06cbca617deafa1d65b518526
collab-virchatri <trilokc@virtusa.com>
2019-08-06 12:58:57 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+)
+
+const PageNotFound = () => (
+  <div>
+    <h1>Page not found</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(section =>
-                        <MenuItem key={section.id} title={section.title} imageUrl = {section.imageUrl} size = {section.size}/>
+                    this.state.sections.map( ({id, ...options}) =>
+                        <MenuItem key={id} {...options}/>
                     )
                 }
 
